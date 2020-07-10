e18e06cb260c1efd12ac98b27c35ff3c1dafe9cc
alaketych <alaketych@ukr.net>
2020-07-10 14:55:55 +0300

    exporting components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter } from './components/index'
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
