e24a9de2a739c8953f992c9a57421dba6c629301
alaketych <alaketych@ukr.net>
2020-07-10 14:48:59 +0300

    App js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import { Header, Main } from './components/index'
+import { Header, Main, Cart, Switch, Route } from './components/index'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+
+      <Switch>
+        <Route path="/" component={ Main } exact></Route>
+        <Route path="/cart" component = { Cart }></Route>
+      </Switch>
     </div>
   );
 }
