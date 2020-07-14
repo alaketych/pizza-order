063164f819a1f8cc5a3f0210b6a7bc9f8ae9cd25
alaketych <alaketych@ukr.net>
2020-07-14 22:01:29 +0300

    App file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import { Header, Main, Cart, Switch, Route } from './components/index'
+import { Header, Switch, Route } from './components/index'
+import { Main, Cart  }           from './components/pages/index'
 
 function App() {
+  const [pizza, setPizza] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/pizza.json')
+      .then(response => response.json())
+      .then(data => {
+        setPizza(data.pizza)
+      })
+  }, [])
+
   return (
     <div className="App">
       <Header />
 
       <Switch>
-        <Route path="/" component={ Main } exact></Route>
-        <Route path="/cart" component = { Cart }></Route>
+        <Route path="/"     render    = { () => <Main items={pizza}/>} exact></Route>
+        <Route path="/cart" component = { Cart } exact></Route>
       </Switch>
     </div>
   );
