c3a9b10e4d609442add8ff1f3e88005295bd2d6c
alaketych <alaketych@ukr.net>
2020-07-18 15:39:06 +0300

    reactjs components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
-export { default as Header }     from './Header'
-export { default as Categories } from './Categories'
-export { default as Item }       from './Item'
-export { default as Sorting }    from './Sorting'
-
-export { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+export { default as Button }        from './Button'
+export { default as Header }        from './Header'
+export { default as Categories }    from './Categories'
+export { default as SortPopup }     from './SortPopup'
+export { default as PizzaBlock }    from './PizzaBlock'
