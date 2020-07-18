1c8ae15eaa95186b3e980fa6f3429c83babf672c
alaketych <alaketych@ukr.net>
2020-07-18 15:40:34 +0300

    redux reducers

--- src/redux/reducers/filters.js
+++ src/redux/reducers/filters.js
@@ -1,19 +1,24 @@
-const { act } = require("react-dom/test-utils")
-
 const initialState = {
-    category: 0,
-    sortBy: 'popular',
+  category: 0,
+  sortBy: 'popular',
 }
 
 const filters = (state = initialState, action) => {
-    if(action.type === 'SET_SORT_BY') {
-        return {
-            ...state,
-            sortBy: action.payload,
-        }
+  if (action.type === 'SET_SORT_BY') {
+    return {
+      ...state,
+      sortBy: action.payload,
     }
+  }
 
-    return state
-}
+  if (action.type === 'SET_CATEGORY') {
+    return {
+      ...state,
+      category: action.payload,
+    }
+  }
+
+  return state
+};
 
 export default filters
