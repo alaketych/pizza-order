96f9a168c30c1628f95c2f52b70a8b8cd3532e8b
alaketych <alaketych@ukr.net>
2020-07-31 20:40:03 +0300

    react pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,46 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchPizzas } from '../redux/actions/pizzas'
-import { setCategory } from '../redux/actions/filters'
+import { setCategory, setSortBy } from '../redux/actions/filters'
 import { Categories, SortPopup, PizzaBlock, PizzaLoader } from '../components'
 
 const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+const sortItems = [
+  { name: 'популярности', type: 'popular' },
+  { name: 'цене',         type: 'price'   },
+  { name: 'алфавит',      type: 'name'    },
+]
 
 function Home() {
   const dispatch = useDispatch()
   const items = useSelector(({ pizzas }) => pizzas.items)
   const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded)
+  const { category, sortBy } = useSelector(( {filters} ) => filters)
 
   useEffect(() => {
-    dispatch(fetchPizzas())
-  }, [])
+    dispatch(fetchPizzas(category, sortBy))
+  }, [category, sortBy])
 
   const onSelectCategory = index => {
     dispatch(setCategory(index))
+  }
+
+  const onSelectSortType = type => {
+    dispatch(setSortBy(type))
   }
 
   return (
     <div className="container">
       <div className="content__top">
         <Categories
-          onClickItem={ onSelectCategory }
-          items={ categoryNames }
+          items = { categoryNames }
+          activeCategory = { category }
+          onClickCategory = { onSelectCategory }
         />
         <SortPopup
-          items={[
-            { name: 'популярности', type: 'popular' },
-            { name: 'цене', type: 'price' },
-            { name: 'алфавит', type: 'alphabet' },
-          ]}
+          items = { sortItems }
+          activeSortType = { sortBy }
+          onClickSortType  = { onSelectSortType }
         />
       </div>
       <h2 className="content__title">Все пиццы</h2>
@@ -39,12 +48,13 @@ function Home() {
         {
           isLoaded
             ? items.map((obj) => (<PizzaBlock key={obj.id} isLoading={true} {...obj} />))
-            : Array(10).fill(<PizzaLoader></PizzaLoader>)
+            : Array(10)
+              .fill(0)
+              .map((_, index) => (<PizzaLoader key={index}></PizzaLoader>))
         }
-
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
